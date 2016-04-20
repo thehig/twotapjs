82bffff439b551b38a4599b8b8fc9728aa649ee7
David Higgins <thehig@gmail.com>
2016-04-20 14:42:20 +0100

    Add the Utilities.js from Dataprovider

--- src/twotapDataProvider.js
+++ src/twotapDataProvider.js
@@ -1,8 +1,15 @@
-var WinJS = require('node-winjs');
+require('./lib/Utilities.js');
 
 WinJS.Namespace.define('Twotapjs', {
-	DataProvider: WinJS.Class.define(function(){}, {}, {})
+	DataProvider: WinJS.Class.derive(XboxJS.Data.DataProvider,
+		function dataprovider_ctor(baseUrl) {
+			// You must call the base class constructor to instantiate a DataProvider.
+			this._baseDataProviderConstructor();
+		}, {
+
+		}
+	)
 });
 
-if(module && module.exports)
+if (module && module.exports)
 	module.exports = Twotapjs;
