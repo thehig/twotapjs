b80d08f2641d7ed5cac5ed9fe9e65eba84969f8c
David Higgins <thehig@gmail.com>
2016-04-22 11:23:06 +0100

    Cleaner array reduce

--- specs/unit/fixtures/list.js
+++ specs/unit/fixtures/list.js
@@ -4,14 +4,8 @@ module.exports = {
 	'all': require('require-dir')()   // defaults to '.'
 	,'all_array': function(){
 		var all = require('require-dir')();
-		return Object.keys(all).map(function(key){
-			return all[key];
-		}).reduce(function(prev, current){
-			// If it's an array, concat to output
-			if(Array.isArray(current)) return prev.concat(current);
-			// If it's an object, push to output
-			prev.push(current);
-			return prev;
+		return Object.keys(all).reduce(function(output, key){
+			return output.concat(all[key]);
 		}, []);
 	}()
 }
