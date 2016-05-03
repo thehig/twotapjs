c2fbd65fb776560257c97e27680d50a9a6988ddb
fguardiola <fernando.guardiola.888@gmail.com>
2016-05-03 16:51:59 +0100

    Add Dataprovider tied to the callback catcher
    * Add callbackcatcher coffee tests
    * Add callbackcatcher dataprovider

--- src/twotapDataProvider.js
+++ src/twotapDataProvider.js
@@ -42,7 +42,7 @@ WinJS.Namespace.define('Twotapjs', {
 					multipleSiteFixtures = sites;
 				}
 			},
-			Cart:{
+			Cart: {
 				DataModel: Twotapjs.Models.CartModel,
 				getCart: function() {
 					// Note: Must return an array
@@ -54,28 +54,99 @@ WinJS.Namespace.define('Twotapjs', {
 			}
 
 		}
+	),
+	CallBackCatcherDataProvider: WinJS.Class.derive(XboxJS.Data.DataProvider,
+		function dataprovider_ctor(baseUrl) {
+			// You must call the base class constructor to instantiate a DataProvider.
+			this._baseDataProviderConstructor();
+		}, {
+			Product: {
+				// 	DataModel: Twotapjs.Models.ProductModel,
+				// 	getProduct: function() {
+				// 		// Note: Must return an array
+				// 		return [singleProductFixture];
+				// 	},
+				// 	setProduct: function(product) {
+				// 		singleProductFixture = product;
+				// 	},
+				// 	getProducts: function() {
+				// 		// Note: Must return an array
+				// 		return multipleProductFixtures;
+				// 	},
+				// 	setProducts: function(products) {
+				// 		multipleProductFixtures = products;
+				// 	}
+				// },
+				// Site: {
+				// 	DataModel: Twotapjs.Models.SiteModel,
+				// 	getSites: function() {
+				// 		// Note: Must return an array
+				// 		return Object.keys(multipleSiteFixtures.sites).map(function(key) {
+				// 			return multipleSiteFixtures.sites[key];
+				// 		});
+				// 	},
+				// 	setSites: function(sites) {
+				// 		multipleSiteFixtures = sites;
+				// 	}
+			},
+			Cart: {
+				DataModel: Twotapjs.Models.CartModel,
+				// getCart: function() {
+				// 	// Note: Must return an array
+				// 	return [singleCartFixture];
+				// },
+
+				getCart: function(cartID) {
+					var tempID = cartID;
+					var baseUrl = "http://callbackcatcher.meteorapp.com/gethit/";
+					var requestUrl = baseUrl + (tempID || "");
+					return Twotapjs.Utilities._requestWrapper(requestUrl);
+				}
+			}
+
+		}
 	)
 });
 
 
 
 WinJS.Namespace.define('Twotapjs.Utilities', {
-	unrecognised: function(source, target, errormessage, whitelist){
+	_defaultTimeout: 30000,
+
+	unrecognised: function(source, target, errormessage, whitelist) {
 		whitelist = whitelist || [];
 
 		var originalKeys = Object.keys(source);
 		var recognisedKeys = Object.keys(target);
-		
+
 		// Add a whitelist of things we don't really care about
 		recognisedKeys = recognisedKeys.concat(whitelist);
 
 		// Give us the 
 		var unrecognisedKeys = originalKeys.filter(function(n) {
-		    return recognisedKeys.indexOf(n) == -1;
+			return recognisedKeys.indexOf(n) == -1;
 		});
 
-		if(unrecognisedKeys.length !== 0) throw new Error(errormessage + unrecognisedKeys);
+		if (unrecognisedKeys.length !== 0) throw new Error(errormessage + unrecognisedKeys);
 		return unrecognisedKeys;
+	},
+	_requestWrapper: function(requestUrl, timeout) {
+		return WinJS.Promise.timeout(timeout ? timeout : Twotapjs.Utilities._defaultTimeout,
+			WinJS.xhr({
+				url: requestUrl,
+				responseType: "json",
+				type: "GET"
+			})
+		).then(
+			function httpSuccess(httpResult) {
+
+				if (httpResult.status == 200) {
+					var parsedresponse = JSON.parse(httpResult.responseText);
+					var responseObject = parsedresponse.body;
+
+					return [responseObject];
+				}
+			});
 	}
 });
 
